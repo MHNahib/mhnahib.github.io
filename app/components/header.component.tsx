53723ff0a3ae875b5dbcd4d0970c6915b1987832
MHNahib <mhnahib@gmail.com>
2025-04-27 10:11:36 +0600

    feat: replace hamburger icon with SVG image for mobile navigation

--- app/components/header.component.tsx
+++ app/components/header.component.tsx
@@ -22,17 +22,14 @@ export const Header = () => {
         <p className="text-2xl font-bold">Hello, </p>
         <nav>
           <button
-            className="lg:hidden flex items-center px-3 py-2 border rounded text-black border-black"
+            className="lg:hidden flex items-center px-3 py-2"
             onClick={handleNavToggle}
           >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            <img
+              src="/assets/burger-menu-right.svg"
+              alt="Menu"
+              className="h-6 w-6"
+            />
           </button>
           <DesktopNav navLinks={navLinks} />
         </nav>

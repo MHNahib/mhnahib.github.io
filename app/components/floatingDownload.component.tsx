16cf9049e23c65b4f651ee7c8caf6dda2abde3d3
M. H. Nahib <47296592+MHNahib@users.noreply.github.com>
2026-06-05 01:08:14 +0600

    Update CV download link in floatingDownload component

--- app/components/floatingDownload.component.tsx
+++ app/components/floatingDownload.component.tsx
@@ -1,7 +1,7 @@
 import { DownloadIcon } from "./icon.component";
 
 const cvURL =
-  "https://drive.google.com/file/d/17fRji5gNNY4hNaNVx5CZaw7VfV_7-huA/view?usp=sharing";
+  "https://drive.google.com/file/d/1glw7_W0tJWJ7THAyYHDFmA1qJsZVtldq/view?usp=sharing";
 
 export const FloatingDownload = () => {
   return (
